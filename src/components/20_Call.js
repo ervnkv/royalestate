1ba60b59d930497323d99c9f81e0ea7a71a0012c
Егор Ревенков <revenkov.egor@gmail.com>
2023-01-14 16:50:59 +0300

    Письма отправляются. Нужна индикация отправки

--- src/components/20_Call.js
+++ src/components/20_Call.js
@@ -1,5 +1,6 @@
 import React,{useEffect} from 'react';
 import './20_Call.scss';
+import emailjs from '@emailjs/browser';
 
 export default function Call ({onClose}) {
 	
@@ -13,21 +14,27 @@ export default function Call ({onClose}) {
 		return () => document.removeEventListener('keydown', handleEscapeKey);
 	}, []);
 
+	const sendMessage = (e) => {
+		e.preventDefault();
+		emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, e.target, process.env.REACT_APP_PUBLIC_KEY);
+	};
+
 	return(
-		<div className='overlay' onClick={onClose}>
-			<div className="f-column overlay-content" onClick={(e) => e.stopPropagation()}>
+		<div className='overlay' onClick={onClose} onSubmit={sendMessage}>
+			<form className="f-column overlay-content" onClick={(e) => e.stopPropagation()}>
 				<h3>Пожалуйста, оставьте Ваши<br></br> контактные данные</h3>
 				<div className='f-column form'>
 					<label>Номер телефона</label>
 					<input type='tel' name='tel' placeholder='+7 000 000 00 00' autoFocus/>
 				</div>
 				<div className='f-column send'>
-					<button onClick={onClose}>
+					{/* <button type='submit' onClick={onClose}> */}
+					<button type='submit'>
 						<h4>Отправить</h4>
 					</button>
 					<p>Наш сотрудник перезвонит Вам в ближайшее время</p>
 				</div>
-			</div>
+			</form>
 		</div>
 	);
 
